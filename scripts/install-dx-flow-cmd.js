b191c20a0db3918267aeb9a6392cd6549afcc7f6
agalakov <agalakov@devexperts.com>
2015-07-28 18:00:00 +0300

    removed root directory check - it does not work anyway

--- scripts/install-dx-flow-cmd.js
+++ scripts/install-dx-flow-cmd.js
@@ -1,17 +1,10 @@
 var shell = require('shelljs');
 var path = require('path');
-var fs = require('fs');
 
 var root = path.resolve(__filename, '..', '..');
 var cwd = process.cwd();
 
-console.log(root, cwd);
-if (root !== cwd) {
-	//link only if not run it own directory
-	var bin = path.resolve(root, '..', '.bin');
+var source = path.resolve(root, 'dx-flow.cmd');
+var destination = path.resolve(root, '..', '.bin', 'dx-flow.cmd');
 
-	var source = path.resolve(root, 'dx-flow.cmd');
-	var destination = path.resolve(root, '..', '.bin', 'dx-flow.cmd');
-	console.log('ln -fs ' + source + ' ' + destination);
-	shell.exec('ln -fs ' + source + ' ' + destination);
-}
+shell.exec('ln -fs ' + source + ' ' + destination);
