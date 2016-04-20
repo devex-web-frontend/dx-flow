a56ca9fee3ee75d0a3c83520f63d48bd6456676b
epavlenko <epavlenko@devexperts.com>
2016-04-20 16:33:53 +0300

    Update compare tickers hook

--- hooks/commit-msg/compareTickers.js
+++ hooks/commit-msg/compareTickers.js
@@ -2,21 +2,20 @@
 
 var fs = require('fs');
 var execSync = require('child_process').execSync;
-
-var commitMsg = fs.readFileSync(process.argv[2]);
+var commitMsg = fs.readFileSync('./.git/COMMIT_EDITMSG');
 var branchName = execSync('git rev-parse --abbrev-ref HEAD').toString();
 
 var branchTicker = getTicker(branchName);
 var commitTicker = getTicker(commitMsg);
 
 if (!!branchTicker && branchTicker !== commitTicker) {
-	console.error(
-		'Error: Branch ticker(' + branchTicker + ') and commit message ticker(' + commitTicker + ') doesn\'t fit');
+	console.error('Error: Branch ticker(' + branchTicker +
+		') and commit message ticker(' + commitTicker + ') doesn\'t fit');
 	process.exit(1);
 }
 
 function getTicker(branchName) {
-	var execResult = /.*[^a-zA-Z0-9]([A-Z]{2,}\-\d{1,})[^a-zA-Z0-9].*/.exec(branchName);
+	var execResult = /([A-Z]{2,}\-\d{1,})/.exec(branchName);
 	return execResult ? execResult[1].toLowerCase() : null;
 }
 
